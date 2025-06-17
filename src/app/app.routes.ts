39693238f9c176752d81c7fa3adaaa1670b27c43
Jummai <jummyhassan5@gmail.com>
2025-06-17 10:45:36 +0000

    add routing and components for post management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './pages/post-list/post-list.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PostListComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'create',
+    component: CreatePostComponent,
+  },
+];
