e0924f01867dfef88f48e943641599d9a31edf18
Jummai <jummyhassan5@gmail.com>
2025-06-22 23:07:21 +0900

    test headers interceptor

--- src/app/interceptors/headers.interceptor.spec.ts
+++ src/app/interceptors/headers.interceptor.spec.ts
@@ -1,17 +1,55 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
-
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { headersInterceptor } from './headers.interceptor';
+import { AuthService } from '../services/auth.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('headersInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => headersInterceptor(req, next));
+  let http: HttpClient;
+  let httpMock: HttpTestingController;
+  let mockAuthService: jasmine.SpyObj<AuthService>;
+
+  const FAKE_TOKEN = 'fake-jwt-token';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockAuthService = jasmine.createSpyObj<AuthService>('AuthService', ['getToken']);
+    mockAuthService.getToken.and.returnValue(FAKE_TOKEN);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        {
+          provide: HTTP_INTERCEPTORS,
+          useValue: headersInterceptor,
+          multi: true,
+        },
+      ],
+    });
+
+    http = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(interceptor).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should add Authorization header when token is present', () => {
+    http.get('/test-endpoint').subscribe();
+
+    const req = httpMock.expectOne('/test-endpoint');
+    expect(req.request.headers.get('Authorization')).toBe(`Bearer ${FAKE_TOKEN}`);
+    req.flush({});
+  });
+
+  it('should not add Authorization header when token is null', () => {
+    mockAuthService.getToken.and.returnValue(null);
+
+    http.get('/test-endpoint').subscribe();
+
+    const req = httpMock.expectOne('/test-endpoint');
+    expect(req.request.headers.has('Authorization')).toBeFalse();
+    req.flush({});
   });
 });
